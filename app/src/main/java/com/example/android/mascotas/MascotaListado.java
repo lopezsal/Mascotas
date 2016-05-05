cadfb3d7d26626843766b85997f480df99e3de26
lopezsal <lopez_4110@hotmail.com>
2016-05-04 23:54:09 -0500

    Initial commit

---
+++ app/src/main/java/com/example/android/mascotas/MascotaListado.java
@@ -0,0 +1,13 @@
+package com.example.android.mascotas;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MascotaListado extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mascota_listado);
+    }
+}
